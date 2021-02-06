612fc0219f823cfe63491cb4c24d9be2b16326ad
SashaCN <sashacn228@gmail.com>
2021-02-06 20:22:59 +0200

    added burger menu

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,5 +1,29 @@
 let profile = document.querySelector(".profile"),
-    profileMenu = document.querySelector(".sing-up")
+    profileMenu = document.querySelector(".sing-up"),
+    //burger menu
+    burger = document.querySelector(".burger"),
+    burgerMenu = document.querySelector(".header-menu"),
+    menuLine = document.querySelector(".header-line"),
+    //header slider (HS)
+    leftHS = document.querySelector(".arrow-left"),
+    rightHS = document.querySelector(".arrow-right")
+
+burger.dataset.activeCheck = "false"
+
+burger.onclick = menu
+
+function menu (){
+  if(burger.getAttribute("data-active-check") == "false"){
+    burger.dataset.activeCheck = "true"
+    menuLine.classList.add("burger-menu-active")
+    burgerMenu.style.display = "block"
+  }else if(burger.getAttribute("data-active-check") == "true"){
+    burger.dataset.activeCheck = "false"
+    menuLine.classList.remove("burger-menu-active")
+    burgerMenu.style.display = "none"
+    burgerMenu.removeAttribute("style")
+  }
+} 
 
 profile.onclick = (event) => {
   event.preventDefault()
