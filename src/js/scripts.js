ad8c7d3d700e8b46827bf0bbda8b2e478aaa2527
SashaCN <sashacn228@gmail.com>
2021-02-07 19:48:45 +0200

    header, about us - adaptive

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -5,10 +5,19 @@ let profile = document.querySelector(".profile"),
     burgerMenu = document.querySelector(".header-menu"),
     menuLine = document.querySelector(".header-line"),
     //header slider (HS)
-    lineHS = document.querySelector(".header-slider"),
-    leftHS = document.querySelector(".arrow-left"),
-    rightHS = document.querySelector(".arrow-right"),
-    imgHS = document.querySelectorAll(".header-slider-img")
+    lineHS = document.querySelector(".photos"),
+    arrLeftHS = document.querySelector(".arrow-left"),
+    arrRightHS = document.querySelector(".arrow-right"),
+    imgHS = document.querySelectorAll(".header-slider-img"),
+    imgWidthHS = imgHS[0].getBoundingClientRect().width,
+    widthHS = Math.round(lineHS.getBoundingClientRect().width),
+    //about us slider 
+    lineAUS = document.querySelector(".events-line"),
+    arrLeftAUS = document.querySelector(".arrow-left-aus"),
+    arrRightAUS = document.querySelector(".arrow-right-aus"),
+    events = document.querySelectorAll(".event"),
+    eventWidthAUS = events[0].getBoundingClientRect().width + 30,
+    widthAUS = Math.round(lineAUS.getBoundingClientRect().width)
 
 burger.dataset.activeCheck = "false"
 
@@ -35,5 +44,63 @@ profile.onclick = (event) => {
   }else{
     profile.classList.add("profile-menu-active")
     profileMenu.style.display = "flex"
+  }
+}
+
+//header slider (HS)
+
+arrLeftHS.onclick = leftHS
+arrRightHS.onclick = rightHS
+arrLeftHS.ondblclick = falseS
+arrRightHS.ondblclick = falseS
+
+function leftHS (event){
+  event.preventDefault()
+  widthHS = Math.floor(widthHS)
+  if(lineHS.scrollLeft == 0){
+    lineHS.scroll(lineHS.scrollWidth, 0)
+  }else{
+    lineHS.scroll(lineHS.scrollLeft-imgWidthHS, 0)
+  }
+}
+
+function rightHS (event){
+  event.preventDefault()
+  widthHS = Math.floor(widthHS)
+  if(Math.round(lineHS.scrollLeft)-10 <= lineHS.scrollWidth - widthHS && Math.round(lineHS.scrollLeft)+10 >= lineHS.scrollWidth - widthHS){
+    lineHS.scroll(0, 0)
+  }else{
+    lineHS.scroll(lineHS.scrollLeft+=imgWidthHS, 0)
+  }
+}
+
+function falseS (){
+  return false;
+}
+
+//about us slider (AUS)
+
+arrLeftAUS.onclick = leftAUS
+arrRightAUS.onclick = rightAUS
+arrLeftAUS.ondblclick = falseS
+arrRightAUS.ondblclick = falseS
+
+function leftAUS (event){
+  event.preventDefault()
+  widthAUS = Math.floor(widthAUS)
+  if(lineAUS.scrollLeft == 0){
+    lineAUS.scroll(lineAUS.scrollWidth, 0)
+  }else{
+    lineAUS.scroll(lineAUS.scrollLeft-eventWidthAUS, 0)
+  }
+}
+
+function rightAUS (event){
+  event.preventDefault()
+  widthAUS = Math.floor(widthAUS)
+  if(Math.round(lineAUS.scrollLeft)-10 <= lineAUS.scrollWidth - widthAUS && Math.round(lineAUS.scrollLeft)+10 >= lineAUS.scrollWidth - widthAUS){
+    lineAUS.scroll(0, 0)
+  }else{
+    lineAUS.scroll(lineAUS.scrollLeft+=eventWidthAUS, 0)
   }
 }
